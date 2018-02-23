b4abd1b287df03a6b0614629ff2a477158a669f7
John Homan <homan.jack@gmail.com>
2018-02-22 20:19:19 -0500

    renamed c and python file to be more descriptive

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup, Extension
 
-extension_mod = Extension("c_gaussian", ["gaussian.c"], undef_macros=['NDEBUG'])
+extension_mod = Extension("c_gaussian", ["row_echelon_transformation.c"], undef_macros=['NDEBUG'])
 setup(name="c_gaussian", ext_modules=[extension_mod])
